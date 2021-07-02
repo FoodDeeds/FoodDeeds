fbe019232b3b34935bb8e325c3dba3630f2c5cd5
Hadeer Yousef <hadeeryousef10@gmail.com>
2021-07-02 12:37:20 -0400

    test

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import "./App.css";
 import db from "./firebase";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Hello World</h1>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hey!</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
